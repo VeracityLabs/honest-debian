9153646c0d8200927a446916107edc8ff68799cf
Clement Heyd <clement.heyd@lincoln.ox.ac.uk>
2024-12-28 00:22:54 +0000

    added complete_build.sh script

---
+++ build_complete.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Run the first script
+# You need to run this as sudo.
+echo "Generating stock raspbian .img with pi-gen"
+bash build.sh
+if [ $? -ne 0 ]; then
+    echo "Error: building pi-gen failed to execute."
+    exit 1
+fi
+
+# Run the second script
+echo "Moving the .img file to custopizer workspace, then renaming to input.img"
+pwd
+sudo mv ./work/raspios-/export-image/pio_image.img. ./CustoPiZer/workspace
+if [ $? -ne 0 ]; then
+    echo "Error: moving the .img file failed."
+    exit 1
+fi
+
+# Run the second script
+echo "Adding the pioreactor files to the .img file"
+cd CustoPiZer
+sudo bash make_worker_image.sh 1.1
+if [ $? -ne 0 ]; then
+    echo "Error: adding pioreactor files failed to execute."
+    exit 1
+fi
+
+echo "pioreactor img file successsfully generated and edited."
